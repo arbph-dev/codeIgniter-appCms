82b117efd614e1e66452fe2297cef22fb028632b
Arnaud <arbph@sfr.fr>
2026-06-17 23:52:02 +0200

    Add files via upload

---
+++ public/assets/js/features/organisation/index.js
@@ -0,0 +1,6 @@
+// js/features/organisation/index.js
+export { initOrgController } from './organisation.controller.js'
+export { initOrgRenderer }   from './organisation.renderer.js'
+export { initOrgForm }       from './organisation.form.js'
+
+
