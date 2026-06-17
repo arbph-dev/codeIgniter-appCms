95fee5a8cfa311b5fec72f86fc35da2591298046
Arnaud <arbph@sfr.fr>
2026-06-17 23:54:01 +0200

    Add files via upload

---
+++ public/assets/js/features/entreprise/index.js
@@ -0,0 +1,4 @@
+// js/features/entreprise/index.js
+export { initEntController } from './entreprise.controller.js'
+export { initEntRenderer }   from './entreprise.renderer.js'
+export { initEntForm }       from './entreprise.form.js'
