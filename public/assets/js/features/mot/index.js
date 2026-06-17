e7f0897770d8e050b94d19ec7efd641e8506fffe
Arnaud <arbph@sfr.fr>
2026-06-17 23:52:40 +0200

    Add files via upload

---
+++ public/assets/js/features/mot/index.js
@@ -0,0 +1,4 @@
+// js/features/mot/index.js
+export { initMotController } from './mot.controller.js'
+export { initMotRenderer }   from './mot.renderer.js'
+export { initMotForm }       from './mot.form.js'
