9d205d17f06d901f27bedadd73b590adcafdfaae
Arnaud <arbph@sfr.fr>
2026-06-17 23:53:39 +0200

    Add files via upload

---
+++ public/assets/js/features/formejuridique/index.js
@@ -0,0 +1,6 @@
+// js/features/formejuridique/index.js
+export { initFjController } from './formejuridique.controller.js'
+export { initFjRenderer }   from './formejuridique.renderer.js'
+export { initFjForm }       from './formejuridique.form.js'
+
+
