bc1f923ad7500d36283d1faee6766c885f123c32
Arnaud <arbph@sfr.fr>
2026-06-18 20:51:04 +0200

    Update SourceInterface.php

--- app/Sources/Contracts/SourceInterface.php
+++ app/Sources/Contracts/SourceInterface.php
@@ -4,24 +4,12 @@ namespace App\Sources\Contracts;
 
 interface SourceInterface
 {
-    /**
-     * Recherche libre.
-     */
-    public function search(
-        string $query,
-        array $options = []
-    ): array;
+    /** Recherche libre     */
+    public function search( string $query, array $options = [] ): array;
+    
+    /** Recherche directe par identifiant*/
+    public function lookup( string $id, array $options = [] ): ?array;
 
-    /**
-     * Recherche directe par identifiant.
-     */
-    public function lookup(
-        string $id,
-        array $options = []
-    ): ?array;
-
-    /**
-     * Nom unique de la source.
-     */
+    /** Nom unique de la source */
     public function getName(): string;
 }
