ac35843dd34b2acfc5b56b48c02f76ca35f77057
Arnaud <arbph@sfr.fr>
2026-06-19 04:18:08 +0200

    Delete app directory

--- app/Views/layouts/footer.php
+++
@@ -1,8 +0,0 @@
-        <!-- FOOTER  
-<footer class="site-footer">           
-        -->
-        <footer>
-            <p>Site powered by CodeIgniter 4, hosted on OVH, full  vanilla</p>             
-        </footer>
-
-
