676edabffd4056babdf4b032ce8152f4c40bfdd8
Arnaud <arbph@sfr.fr>
2026-07-02 00:42:55 +0200

    Add files via upload

---
+++ old/app/Views/pages/technologies/vap/requalification.php
@@ -0,0 +1,214 @@
+<!-- ==================================================================================================================================== -->
+
+<div id="index" class="tabcontent" style="display: block;">
+
+    <article class="cp_soft-card">
+        <header>
+            <h1>Requalification périodique</h1>
+            <p>La requalification périodique ou décennale des générateurs de vapeur doit être réalisé avec un organisme de contrôle. 
+
+            </p>
+        </header>
+
+    <!-- 
+        ==================================================================================================================================== 
+    -->  
+        <section>
+            <h2>Préparation de la requalification</h2>
+            <div> 
+                <div>
+                    <p>
+                        La préparation de la requalification comprend
+                        <ul>
+                            <li>L'inventaire des taches et la consultation des fournisseurs</li>
+                            <li>La commandes des prestations et matériels</li>
+                            <li>La planification des interventions</li>
+                            <li>La préparation des raccordements: électricité, eau, air ,vapeur, condensat</li>
+                            <li>Le suivi des commandes et la réception des matériels</li>
+                            <li>L'implantation du générateur de secours</li>
+                            <li>les raccordements avec diminution de la production</li>
+                            <li>les essais de sécurité, les réglages de régulation</li>
+                            <li>la réception de l'installation provisoire par l'organisme de contrôle</li>
+                            <li>La formation du personnel autorisé a réaliser les tests de sécurité</li>
+                        </ul>
+                        Avant de planifier les interventions il faut s'assurer des délais auprès des fournisseurs.<br>
+                        Une inspection  minutieuse des équipements est nécessaire pour relever les composants à remplacer et noter les réparations à effectuer.<br>
+                        Ci contre une soudure endommagée, probablement par laminage, a conduit à remplacer les "manchettes" sur la vanne de régulation d'eau alimentaire<br>
+                        <br>
+                        Quelques points ont nécessité une attention particulière et quelques discussions.<br>
+                        Les asservissements implantés ont posé des difficultés, aucune possibilité de réguler le niveau. Le personnel était "habitué" à un réglage manuel. La solution préconisée imposant le passage de câble fut abandonnée.<br>
+                        Le réglage des régulations fut dégrossi et devait être peufiné avec les courbes de production sur le superviseur. Le conducteur devait gérer ses réglages.<br>
+                        <br>
+  
+                        <h3>Générateur de secours</h3>
+                        La requalification des deux générateurs de vapeur devait s'étaler sur 3 semaines avec une semaine de battement pour les surprises éventuelles.<br>
+                        <br>
+                        Les choses sérieuses commencent avec la manutention du généateur de location.<br>
+                        Le conduit de cheminée est implanté.Les raccordements, préparés en amont, ne posent pas de difficultés.<br>
+                        
+                        <br>
+                        La mise en service par le spécialiste commence. <br>
+                        Ci contre Le bruleur du générateur de vapeur, on distingue la ligne gaz avec le bloc gaz et son contrôleur d'étanchéité.<br> 
+                        A l'opposé l'armoire de contrôle et de commande. Le ventilateur en noir se trouve devant le bruleur.<br>
+                        On teste les asservissements et le déclenchement des sécurités. Durant ces essais nous échangeons pour prendre en main les équipements.<br>                         
+                        La chaudière est fonctionnelle, l'organimse de controle valid el'instalaltion du générateur de location. Le raccordement au réseau vapeur se fera le lendemain. <br>
+                        <br>
+                        La préparation est bien avancée<br>
+                        Je forme ensuite le personnel à la conduite et leur fait réaliser la séquence de test.
+
+
+                        <br>
+
+                    </p>
+                </div>
+
+                <aside>
+                    <img src="/assets/img/technologies/vap/entremont/vap_requalprep1.png" width="350">
+                    
+                    
+                    
+                    <img src="/assets/img/technologies/vap/entremont/vap_requalprep2.png" width="350">                        
+                    
+                    
+                    <img src="/assets/img/technologies/vap/entremont/vap_requalprep3.png" width="350">                        
+                    
+
+                </aside>                
+            </div>
+        </section>
+
+
+
+
+    <!-- 
+        ==================================================================================================================================== 
+
+    -->  
+
+        <section>
+            <h2>Requalification</h2>
+            <div> 
+                <div>
+                La préparation et les raccordements sont terminés, le temps restant aurait permis au conducteur de peaufiner les réglages des consignes tout en disposant toujours de 2 générateurs si besoin.<br>
+                Le balisage est installé le vendredi.<br>
+                <br>
+                On arrête le générateur. On l'isole des réseaux : vapeur, eau, condensat, électricité, air comprimé puis on procède aux consignations.<br>
+                Le générateur doit refroidir, on ouvre les portes en face avant<br>
+                <br>
+                Les premiers éléments sont remplacés :
+                •	contacteurs<br>
+                •	pressostat gaz<br>
+                •	pompe alimentaire<br>
+                <br>
+                A l'origine le bruleur ne disposait pas de contrôle d'étanchéité indispensable sur cette puissance.<br>
+                Pour moderniser la ligne gaz le constructeur, a l'époque.., a choisi la solution tout pressostat sans évent.<br>
+                On trouve souvent un bloc gaz, avec controleur d'étanchéité, plus simple et surtout plus fiable..<br>
+                La remise en service a été assez compliqué. 
+
+                <h3>Ouverture du générateur, coté feu</h3>
+                
+                Dés le lundi matin les soudeurs attaquent le ressuage. 4 tubes sont à revoir dont 2 à remplacés.<br>
+                •	La soudure est meulé des deux cotés. <br>
+                •	Le tube est sortie et découpé en allant.<br>
+                •	Le nouveau tube de 6m est mis en place d'un tenant. <br>
+                •	Il est coupé a longueur puis pointé.<br>
+                •	Il est ensuite dudgeonné des 2 cotés. <br>
+                •	Les tubes sont ressoudés sur les plaques tubulaires<br>
+                •	Les tresses des portes sont remplacées après nettoyage<br>
+                <br>
+                Les soudeurs confirmés m'ont assurés être a même de faire le diagnostique après ressuage. l'organisme de contrôle voulait intervenir à ce moment,il facture chaque magnétoscopie...<br>
+                Il est intervenu après réparation pour vérifier les soudures réparées.<br>
+                <br>
+                Le ressuage à fait apparaitre une fissure dans un tube à l'arrière du générateur.<br>
+                On voit au dessus la trace d'une réparation précédente.<br>
+                <br>
+                La fissure étant prononcée le risque de rupture étant avéré on pouvait bouchonner ou remplacer le tube.<br>
+                La fissure provenant d'un défaut du tube et ou des contraintes due a l'exploitation on a choisit de remplacer le tube.<br>
+                Ce tube fera "ressort" pour la dilatation et fusible en évitant d'endommager la plaque tubulaire.<br>
+                L'organisme de contrôle consulté a fin de conseil a valider le choix.<br><br>
+                Il prit note des remarques quant au contrainte thermiques due a l'exploitation. Cycle de dilation rétractation due au variations importantes de charge<br>
+                <br>
+                Il faut encore préparer l'épreuve hydraulique du lendemain...
+
+                <h3>Ouverture du générateur, coté eau</h3>
+                Les composants de sécurité sont déposés, les rampes de pression sont vérifiées et nettoyées.<br>
+                Les montures de niveau sont retirées ainsi que les niveaux autocontrôlés et la vanne de régulation<br>
+                On ferme les piquages avec des brides pleines.<br>
+                On laisse un évent et un piquage pour le remplissage.<br>
+                On monte les nouvelles soupapes sur la chaudière et on contrôle le matériel<br>
+                On est prêt pour l'épreuve hydraulique de l'après midi.<br>
+                <br>
+                L'épreuve hydraulique se fait à une pression de 1,5 x le timbre du générateur; soit 27 bar pour un générateur de 18 Bar.
+
+                <h3>Epreuve hydraulique et magnétoscopie</h3>
+                Après l'épreuve hydraulique, la magnétoscopie révélera tout de même un défaut mineure sur une réparation. L'inertage d'un tel volume n'est pas aisée.<br>
+                La soudure reprise et une gracieuse magnétoscopie plus tard la chaudière est déclarée conforme.<br>
+                <br>
+                Maintenant il reste tout a remonter pour valider les sécurités avant l'arrêt du second générateur.<br>
+                On vidange; on remonte et on met en eau.<br>
+                <br>
+                On rédige les documents: bon de travaux et procès verbal de requalification.<br>
+                Les soudeurs ont finis leur semaine, bien chargé, et l'organisme de contrôle veut les certificats de tarage des nouvelles soupapes et le procès verbal des réparations
+
+                <h3>Vérification des sécurités</h3>
+                La dernière étape est la vérification des sécurités, cette vérfication s'est bien déroulée.<br>
+                Le remplacement du pressostat de controle d'étanchéité gaz, a solutionné un défaut récurrent lors des tests
+
+                <h3>Travaux et traçabilité</h3>
+                Les équipements sous pression sont soumis a des réglementations spécifiques.<br>
+                Chaque générateur doit disposer d'un dossier technique à jour. On doit retrouver une trace des réparations effectuées<br>
+                Le dossier technique indique les réparations précédentes et le procès verbal de requalification celle réalisés lors de la requalification.<br>
+                Ces documents sont archivés à la maintenance et transmis au service qualité pour enregistrement.<br>
+
+                Le générateur est reparti pour 10ans...
+                </div>
+
+                <aside>
+
+                    <img src="/assets/img/technologies/vap/entremont/vap_requal1.png" width="350">
+
+
+                    <div id="CAROUSEL_1" class="cp_carousel">
+                        <img src="/assets/img/technologies/vap/entremont/vap_requal2.png">
+                        <img src="/assets/img/technologies/vap/entremont/vap_requal3.png">
+                        <img src="/assets/img/technologies/vap/entremont/vap_requal4.png">
+                    </div>
+                    <button onclick="ihmCarouselPrev('1')">‹</button>
+                    <button onclick="ihmCarouselNext('1')">›</button>
+
+                    <div id="CAROUSEL_3" class="cp_carousel">
+                        <img src="/assets/img/technologies/vap/entremont/vap_requal5.png">
+                        <img src="/assets/img/technologies/vap/entremont/vap_requal6.png">
+                    </div>
+                    <button onclick="ihmCarouselPrev('3')">‹</button>
+                    <button onclick="ihmCarouselNext('3')">›</button>                    
+
+                    <div id="CAROUSEL_4" class="cp_carousel">
+                        <img src="/assets/img/technologies/vap/entremont/vap_requal7.png">
+                        <img src="/assets/img/technologies/vap/entremont/vap_requal8.png">
+                    </div>
+                    <button onclick="ihmCarouselPrev('4')">‹</button>
+                    <button onclick="ihmCarouselNext('4')">›</button>  
+
+
+            <!-- 
+                    <h3>Consignations</h3>
+                    Les intervenants sont peu nombreux, quatre groupes interviendront conjointement.<br>
+                    La zone d'intervention doit être balisée, les EPI sont strictement recommandés dans la zone de travail.<br> 
+                    Les équipements sont consignés et les travaux peuvent débuter <br>                    
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal1.png
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal2.png
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal3.png
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal4.png
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal5.png
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal6.png
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal7.png
+sftp://rxrhgiw@ftp.cluster100.hosting.ovh.net/home/rxrhgiw/www/public/assets/img/technologies/vap/entremont/vap_requal8.png
+
+    -->  
+                </aside>                
+            </div>
+        </section>
+    </article>            
+
+
