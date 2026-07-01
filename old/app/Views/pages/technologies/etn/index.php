9caea85a162cd4066ebf4bfc1e22827ed542dfb2
Arnaud <arbph@sfr.fr>
2026-07-02 00:37:07 +0200

    Create index.php for Electrotechnique section
    Added index.php file for Electrotechnique section with various links and structure.

---
+++ old/app/Views/pages/technologies/etn/index.php
@@ -0,0 +1,65 @@
+<!-- 
+"G:\WEB\BACKUP\elfennel.fr\public_html\cv\VAE\RSC\Organisations\BEAUVAIS\Spontex\Armoire Pompe condensats.html"
+                    
+   "G:\WEB\BACKUP\elfennel.fr\public_html\cv\VAE\RSC\Organisations\BEAUVAIS\Spontex\CondensateurHTA.html"                 
+
+-->
+
+
+<div id="index" class="tabcontent" style="display: block;">
+
+    <article class="cp_soft-card">
+        <header>
+            <h1>Electrotechnique</h1>
+            <p>
+            <br>
+             </p>
+        </header>
+
+        <section>
+            <h2></h2>
+            <div>
+                <div>
+<!-- 
+
+                    
+                    
+
+-->
+
+                </div>
+                <aside>
+
+                </aside>                
+            </div>
+        </section>
+
+
+    </article>        
+
+</div>
+
+
+  id="ETN000"  Electrotechnique
+  id="ETN001" ETN - Centrale électrique
+
+  id="ETN002" ETN - Distribution électique
+
+      <ul>
+        <li><a href="https://elfennel.fr/cv/rwd.html#ETN002A">ETN002A - Postes HTA</a>  
+          <ul>
+            <li><a href="https://elfennel.fr/cv/rwd.html#ETN002A">ETN002A - Postes HTA</a></li>
+            <li><a href="https://elfennel.fr/cv/rwd.html#ETN002B">ETN002B - Cellules haute tension</a></li>
+          </ul>
+        </li> 
+      </ul> 
+
+
+
+              <ul>
+          <li><a href="https://elfennel.fr/cv/rwd.html#ETN001A">ETN001A - Poste HTA</a></li>
+          <li><a href="https://elfennel.fr/cv/rwd.html#ETN001B">ETN001B - Salle de contôle</a></li>
+          <li><a href="https://elfennel.fr/cv/rwd.html#ETN001C">ETN001C - Salle des machines</a></li>
+          <li><a href="https://elfennel.fr/cv/rwd.html#ETN001D">ETN001D - Exploitation</a></li>
+          <li><a href="https://elfennel.fr/cv/rwd.html#ETN001E">ETN001E - procédure : Défaut groupe ou centrale</a></li>
+          <li><a href="https://elfennel.fr/cv/rwd.html#ETN001E">ETN001F - procédure : Essai à vide</a></li>
