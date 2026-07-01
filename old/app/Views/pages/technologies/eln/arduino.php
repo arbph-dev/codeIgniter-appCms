5e2c3148e0a1a3e617ebf9d73bf8db0d7d90610c
Arnaud <arbph@sfr.fr>
2026-07-02 00:34:29 +0200

    Add files via upload

---
+++ old/app/Views/pages/technologies/eln/arduino.php
@@ -0,0 +1,141 @@
+<div id="index" class="tabcontent" style="display: block;">
+
+    <article class="cp_soft-card">
+        <header>
+            <h1>Arduino</h1>
+            <p>
+                 Le projet Arduino permet de réaliser des prototypes, de se familiariser avec l'électronique numérique et la programmation temps réel.  
+            </p>
+        </header>
+            
+            <!--SECTION  -->
+            <section>
+                <h2>Arduino MEGA</h2>
+                <div>
+                    <div>
+                        <h3>ATmega2560</h3> 
+                        Ce composant dispose de 100 Broches, il est compatible avec les niveaux TTL.<br>
+                        <br>
+                        L'ATmega2560 est un microcontonleur 8 bits, les broches d'entrées et sorties sont réparties sur des ports.<br>
+                        Les broches peuvent avoir plusieurs fonctions, fonctionner en entrées ou en sortie selon la configuration retenue.<br>
+                        <br>
+
+                        <h4>Caracétristiques de l'Arduino Mega</h4>
+                        Tension de fonctionnement 5V<br>
+                        Broche d'entrées sorties : 54<br>
+                        Entrées analogiques	16<br>
+                        Memoire Flash 256 KB ,SRAM 8 Ko, EEPROM 4 Ko<br>
+                        Fréquence d'horloge 16 MHz<br>
+                    </div>
+                    <aside>
+                        <a href="http://www.atmel.com/Images/Atmel-2549-8-bit-AVR-Microcontroller-ATmega640-1280-1281-2560-2561_datasheet.pdf" target="_blank">
+                        La documentation technique du microcontroleur
+                        </a>
+
+                        <img src="/assets/img/technologies/eln/megaboard.jpeg" width="300"/><br/>
+
+                        <a href="/assets/img/technologies/eln/pincolor2560.jpg" target="_blank" alt="capacité d'échange selon Th et TAC">
+                            <img src="/assets/img/technologies/eln/pincolor2560.jpg" width="300"/>
+                        </a><br/>                        
+                    </aside>                
+                </div>
+            </section>
+
+            <!--SECTION  -->
+            <section>
+                <h2>Shield</h2>
+                <div>
+                    <div>
+                        <h3>Shield Ethernet W5100 Carte SD</h3>
+                        <p>
+                            Les shields sont des cartes enfichables permettant d'étendre les fonctions de l'Arduino Mega<br>
+                            La version actuelle est disponible<a href="https://store.arduino.cc/products/arduino-ethernet-shield-2" target="_blank">ici</a><br>
+                            <br>
+                            Cette carte permet de disposer d'une interface ethernet compatible POE et une carte SD.<br>
+                            <br>
+                            Cette carte m'a permit de réaliser un esclave ModbusTCP.<br>
+                            Un projet de passerelle Modbus viendra compléter cette rubrique.
+                        </p>
+                    </div>
+                    <aside>
+
+                        <a href="/assets/img/technologies/eln/shieldwd500.jpg" target="_blank" alt="carte extension ethernet pour arduino">
+                            <img src="/assets/img/technologies/eln/shieldwd500.jpg" width="300"/>
+                        </a>
+
+
+                    </aside>                
+                </div>
+            </section>
+            
+            <!--SECTION  -->
+            <section>
+                <h2>Carte dérivées</h2>
+                <div>
+                    <div>
+                        <h3>Mutliwii Mega (Hobbyking)</h3>
+                        <p>
+                            Ce contrôleur de vol avait été crée sur une plateforme Arduino et supporté par le projet MultiWii.<br>
+                            Il m'a permis de réaliser mon premier quadricoptère.<br>
+                            <br>
+                            Le microcontroleur est un Atmega 2560<br>
+                            <br>
+                            La carte est doté de composants additionnels:<br>
+                            ITG3205 gyroscope<br>
+                            BMA180 accéléromètre<br>
+                            HMC8553 compas<br>
+                            BMA085 baromètre<br>
+                            MTK3329 GPS<br> 
+                        </p>
+                    </div>
+                    <aside>
+                        <a href="/assets/img/technologies/eln/megamwii.jpg" target="_blank" alt="capacité d'échange selon Th et TAC">
+                            <img src="/assets/img/technologies/eln/megamwii.jpg" width="300"/>
+                        </a><br/><br/>
+
+                    </aside>                
+                </div>
+            </section>
+
+    </article>        
+
+</div>
+
+
+
+
+
+      
+      <!--SECTION  
+      /assets/img/technologies/eln/bcounter16_block.png
+/assets/img/technologies/eln/bcounter16_block_low.png
+/assets/img/technologies/eln/codeISR.png
+/assets/img/technologies/eln/codeISR_low.png
+/assets/img/technologies/eln/codePWM.png
+/assets/img/technologies/eln/codePWM_low.png
+/assets/img/technologies/eln/counterTiming.png
+/assets/img/technologies/eln/counterTiming_low.png
+/assets/img/technologies/eln/Mega-Pinout-Diagram.png
+
+/assets/img/technologies/eln/megamwii.jpg
+
+/assets/img/technologies/eln/PinMap2560_hi.png
+/assets/img/technologies/eln/PinMap2560_low.png
+/assets/img/technologies/eln/pwm_principe.png
+/assets/img/technologies/eln/pwm_principe1.png
+/assets/img/technologies/eln/pwm_principe2.png
+/assets/img/technologies/eln/regPCICR.png
+/assets/img/technologies/eln/regPCICR_low.png
+/assets/img/technologies/eln/regPCMSK2.png
+/assets/img/technologies/eln/regPCMSK2_low.png
+/assets/img/technologies/eln/regPwmCS.png
+/assets/img/technologies/eln/regPwmCS_low.png
+/assets/img/technologies/eln/regTCCR3B.png
+/assets/img/technologies/eln/regTCCR3B_low.png
+/assets/img/technologies/eln/shieldwd500.jpg
+      -->
+
+
+
+
+
