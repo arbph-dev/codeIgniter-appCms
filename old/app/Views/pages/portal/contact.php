9bf2a39060e9ae392c972d0e6792c7a81d524903
Arnaud <arbph@sfr.fr>
2026-07-02 00:45:22 +0200

    Add contact.php with initial contact information

---
+++ old/app/Views/pages/portal/contact.php
@@ -0,0 +1,34 @@
+        <article class="cp_soft-card">
+            <header>
+                <h1>Arnaud BOIS PORHEL</h1>
+                <p>Je réside dans le Finistère Sud</p>
+            </header>
+        
+            <!-- Article 1 Section 1 
+                    <h1>Contact</h1>
+        <h3>Arnaud BOIS PORHEL</h3>
+        
+
+        <p>Bonjour</p> 
+            -->
+            
+            <section>
+                <h2>Coordonnées</h2>
+                <div>
+                    <div>
+                        EN construction
+                    </div>
+                    
+                    <aside>
+                        <ul>
+                        <li><a href="#">Adresse mail</a></li>
+                        
+                        </ul>
+                    </aside>                
+
+                </div>
+            </section>
+
+
+
+        </article>
