81b79c91ead67ae7cf4eedfd0444e36e25ee2621
Arnaud <arbph@sfr.fr>
2026-07-02 00:27:57 +0200

    Create CAN technology page
    Add CAN technology page with telecommunications information.

---
+++ old/app/Views/pages/technologies/com/can.php
@@ -0,0 +1,49 @@
+<!-- ==================================================================================================================================== -->
+
+<div id="index" class="tabcontent" style="display: block;">
+
+    <article class="cp_soft-card">
+        <header>
+            <h1>Télécommunications</h1>
+            <p>
+            Les réseaux présentés sont des réseaux d'automatisme, de supervision et de télégestion.
+                <br>
+
+            </p>
+        </header>
+
+    <!-- ==================================================================================================================================== 
+## CAN
+```
+I:\VAE\OLD\ATRIER\PriusCAN project.doc
+
+```
+voir la note [[ALL/TRIES/communications/can/source-PriusCAN]], retrouver le code source #revoir
+
+----
+
+        <a href="/assets/img/technologies/com/carteapplicom_h.png" target="_blank">
+            <img src="/assets/img/technologies/com/carteapplicom_l.png" width="350">
+        </a>
+        <br>
+
+        -->  
+        <section>
+            <h2></h2>
+            <div> 
+<br>
+                </div>
+                <aside>
+
+
+                   
+                </aside>                
+            </div>
+        </section>
+
+
+    </article>            
+
+  
+
+</div>
