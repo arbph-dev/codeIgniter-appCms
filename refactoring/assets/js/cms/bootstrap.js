b49fcddc1e6ef2e57b0983ebbc371f219098651f
Arnaud <arbph@sfr.fr>
2026-07-07 09:07:15 +0200

    Add initCallout to CMS initialization

--- refactoring/assets/js/cms/bootstrap.js
+++ refactoring/assets/js/cms/bootstrap.js
@@ -1,10 +1,12 @@
 import { initApex } from '/assets/js/components/apex.js'
 import { initCodeVal } from '/assets/js/components/codeval.js'
 import { initMermaid } from '/assets/js/components/mermaid.js'
+import { initCallout} from '/assets/js/components/callout.js'
 
 export function initCms()
 {
     initApex()
     initMermaid()
     initCodeVal()
+    initCallout()
 }
