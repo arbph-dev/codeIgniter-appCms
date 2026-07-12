d00df4a9a60c8a0bfd1d2424bb3a3c11bb64db40
Arnaud <arbph@sfr.fr>
2026-07-13 01:49:37 +0200

    Refactor CMS component initialization
    Refactor initialization order of CMS components for clarity and dependency management.

--- refactoring/assets/js/cms/bootstrap.js
+++ refactoring/assets/js/cms/bootstrap.js
@@ -1,12 +1,36 @@
+// /assets/js/cms/bootstrap.js
+
+
 import { initApex } from '/assets/js/components/apex.js'
-import { initCodeVal } from '/assets/js/components/codeval.js'
-import { initMermaid } from '/assets/js/components/mermaid.js'
 import { initCallout} from '/assets/js/components/callout.js'
+import { initCodeVal } from '/assets/js/components/codeval.js'
+import { initLeaflet }  from '/assets/js/components/leaflet.js'
+import { initMermaid } from '/assets/js/components/mermaid.js'
+import { init } from `/assets/js/components/three/index.js`
+
+/*
+    ATTENION : ordre de dépendance
+    - CodeVal publie des événements apex:render, donc Apex doit être initialisé avant.
+        on prévoit de faire de meme avec d'autres composants 
+        CodeVal est intilalisé en dernier
+
+    - Callout , Leaflet , Mermaid peuvent être initialisés dans n'importe quel ordre.    
+*/
 
 export function initCms()
 {
+    // dépendance de CodeVal
     initApex()
-    initMermaid()
-    initCodeVal()
+
+    // independant
     initCallout()
+    initLeaflet()    
+    initMermaid()
+
+    init()
+
+    // utilisateur de composant
+    initCodeVal()
+
+
 }
