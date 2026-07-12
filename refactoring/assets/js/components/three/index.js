14db6db75c0a5cbddb0a549f71f2208812fe6edc
Arnaud <arbph@sfr.fr>
2026-07-12 21:43:53 +0200

    Add documentation comments to index.js
    Added documentation comments for the Three.js component.

--- refactoring/assets/js/components/three/index.js
+++ refactoring/assets/js/components/three/index.js
@@ -1,1 +1,13 @@
+/*
 
+/assets/js/components/three/index.js
+
+- API publique
+- découverte des `.cp_threejs`
+- lecture de `data-options`
+- création des instances
+- destruction
+- rafraîchissement
+
+Aucune connaissance de Three.js.
+*/
