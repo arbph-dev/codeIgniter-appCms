0d6204735c478894dca6f41af1ae2808fdca1b8e
Arnaud <arbph@sfr.fr>
2026-07-15 00:35:12 +0200

    Add files via upload

---
+++ refactoring/assets/js/components/modelworkbench/index.js
@@ -0,0 +1,14 @@
+/**
+    /assets/js/components/modelworkbench/index.js
+
+*/
+
+import { ModelWorkbench } from '/assets/js/components/modelworkbench/ModelWorkbench.js'
+    
+/**
+ * Initialise le ModelWorkbench (Administration).
+ */
+export function initModelWorkbench()
+{
+    new ModelWorkbench();
+}   
