4e02b187427390a7accfa03235b744e6f3ab2297
Arnaud <arbph@sfr.fr>
2026-07-29 22:20:19 +0200

    Create ComponentDefinition class for components
    Add ComponentDefinition class to define component metadata.

---
+++ refactoring/app/Libraries/Components/ComponentDefinition.php
@@ -0,0 +1,26 @@
+<?php
+// app/Libraries/Components/ComponentDefinition.php
+namespace App\Libraries\Components;
+
+/**
+ * Définition d'un composant.
+ *
+ * Cette classe décrit les métadonnées techniques d'un composant.
+ *
+ * Décision : D014
+ */
+final class ComponentDefinition
+{
+    public function __construct(
+        public string  $type,
+        public string  $description,
+        public string  $descriptorClass,
+        public string  $rendererClass,
+        public string  $adminRendererClass,
+        public array   $resources = [],
+        public array   $features = [],
+        public array   $connectors = [],
+        public ?string $workbenchClass = null,
+        public array   $metadata = [],
+    ) {}
+}
