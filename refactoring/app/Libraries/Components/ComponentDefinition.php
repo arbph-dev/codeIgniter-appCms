33186ad57d92c96fe270edbca5d43769d22d149b
Arnaud <arbph@sfr.fr>
2026-07-31 04:19:08 +0200

    Add new properties to ComponentDefinition constructor

--- refactoring/app/Libraries/Components/ComponentDefinition.php
+++ refactoring/app/Libraries/Components/ComponentDefinition.php
@@ -14,13 +14,21 @@ final class ComponentDefinition
     public function __construct(
         public string  $type,
         public string  $description,
+
+        public string  $label,
+        public string  $icon,
+        public string  $cssClass,
+
         public string  $descriptorClass,
         public string  $rendererClass,
         public string  $adminRendererClass,
+
         public array   $resources = [],
         public array   $features = [],
         public array   $connectors = [],
+        
         public ?string $workbenchClass = null,
+        
         public array   $metadata = [],
     ) {}
 }
