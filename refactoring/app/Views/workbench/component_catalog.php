7e2b7de0a113f165bf9362f5636927d2e0605c1e
Arnaud <arbph@sfr.fr>
2026-08-01 17:23:19 +0200

    Add component_catalog.php for catalog workbench

---
+++ refactoring/app/Views/workbench/component_catalog.php
@@ -0,0 +1,10 @@
+<div id="catalogWorkbench"></div>
+
+<script type="module">
+import ComponentCatalogWorkbench
+from '/assets/js/ui/workbench/catalog/ComponentCatalogWorkbench.js';
+
+const wb = new ComponentCatalogWorkbench();
+
+await wb.init('#catalogWorkbench');
+</script>
