1014cd5eeb4b3a05cfd7615e1278a0bec8723aee
Arnaud <arbph@sfr.fr>
2026-08-24 08:19:04 +0200

    Create store.py

---
+++ project/sysex/XXpertSystem/features/codenaf/store.py
@@ -0,0 +1,11 @@
+"""
+features/codenaf/store.py
+"""
+codenaf_store = {
+    "data":       [],
+    "loading":    False,
+    "error":      None,
+    "q":          None,
+    "code":       None,
+    "pagination": {"currentPage": 1, "perPage": 10, "total": 0},
+}
